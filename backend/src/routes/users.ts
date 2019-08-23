2a5d44d028f3b8b1f902c8da6140a1d2ef55385f
N. Justus <nicola.justus@mni.thm.de>
2019-08-23 18:10:49 +0200

    enable creating users

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,19 @@
 import express from "express";
 const usersRouter = express.Router();
+import {UserStore} from "../stores/index";
 
 /* GET users listing. */
-usersRouter.get("/", (req, res) => {
-  res.json({name: "nico", age: 25});
+usersRouter.get("/", async (req, res) => {
+  const users = await UserStore.find();
+  res.json(users);
+});
+usersRouter.post("/", async (req, res) => {
+  const user = new UserStore(req.body);
+  res.json(await user.save());
+});
+usersRouter.get("/:userName", async (req, res) => {
+  const user = await UserStore.find({username: req.params.userName});
+  res.json(user);
 });
 
 export default usersRouter;
--- backend/src/stores/user-store.ts
+++ backend/src/stores/user-store.ts
@@ -2,6 +2,13 @@ import mongoose, {Schema} from "mongoose";
 import {User} from "../../../interfaces/index";
 
 export const UserSchema = new Schema({
-  username: String,
+  username: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
   displayName: String
 });
+
+export const UserStore = mongoose.model<User>("User", UserSchema);
