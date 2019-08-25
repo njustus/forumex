f43bc583fed59b433fbb36728265a3590eb16eda
N. Justus <nicola.justus@mni.thm.de>
2019-08-25 20:37:12 +0200

    render all messages for a thread

--- frontend/src/app/thread/thread.component.ts
+++ frontend/src/app/thread/thread.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { flatMap, map } from 'rxjs/operators';
-import { ThreadService } from '../services/thread.service';
+import { ThreadService, MessageService } from '../services/index';
 import { Observable } from 'rxjs';
-import { Thread } from '../../../../interfaces';
+import { Thread, Message } from '../../../../interfaces';
 
 @Component({
   selector: 'app-thread',
@@ -12,13 +12,16 @@ import { Thread } from '../../../../interfaces';
 })
 export class ThreadComponent implements OnInit {
   private thread: Observable<Thread>
+  private messages: Observable<Message[]>
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly threadService: ThreadService) {}
+    private readonly threadService: ThreadService,
+    private readonly messageService: MessageService) {}
 
   ngOnInit() {
     this.thread = this.route.paramMap
       .pipe(map(params => params.get('threadId')))
       .pipe(flatMap(id => this.threadService.getThread(id)))
-    }
+    this.messages = this.thread.pipe(flatMap(thread => this.messageService.forThread(thread._id.toString())))  
+  }
 }
