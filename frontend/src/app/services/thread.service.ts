218bb9d73d891bb7b0fd0d3ec38650034bfbf2c7
N. Justus <nicola.justus@mni.thm.de>
2019-08-31 14:43:33 +0200

    add 'createThread'

--- frontend/src/app/services/thread.service.ts
+++ frontend/src/app/services/thread.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Thread } from '../../../../interfaces'
+import {FormThread} from '../models/index'
+import { testUser } from './index';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +20,8 @@ export class ThreadService {
     uri.searchParams.append('page', encodeURIComponent(page))
     console.log("calling: ", uri)
     return this.http.get<Thread[]>(uri.toString())
+  }
+  createThread(t:FormThread): Observable<Thread> {
+    return this.http.post<Thread>("/api/threads", {...t, author: testUser})
   }
 }
